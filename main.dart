90028a55055c0ffe9e9fdc1126dbdd8915347277
joma gsm <jomagsm@gmail.com>
2021-03-11 01:00:01 +0600

    add compGuess noob

--- main.dart
+++ main.dart
@@ -2,30 +2,20 @@ import 'dart:io';
 
 import 'dart:math';
 
-void main(){
-    var hiddenNumber = getRandomNumber(1, 101);
-    print(hiddenNumber);
-    for(;;){
-    stdout.write("Введите число: ");
-    int inputUserNumber = int.parse(stdin.readLineSync()!);
-    var answer = comparisonOfNumber(hiddenNumber,inputUserNumber);
-    if (answer == 'OK'){
-        print('Вы выиграли');
-        break;
-    }
-    else{
-        print('Число $answer загаданного');
-    }
-    }
-    // var name = stdin.readLineSync();
-    // stdout.write(name);
-    //
-    // void printValue(int value){
-    //     print(value);
-    // }
-    // printValue(123);
-    //
-    //     var number = 0;
+void main() {
+  var hiddenNumber = getRandomNumber(1, 101);
+
+  // userGuess(hiddenNumber);
+  compGuess();
+  // var name = stdin.readLineSync();
+  // stdout.write(name);
+  //
+  // void printValue(int value){
+  //     print(value);
+  // }
+  // printValue(123);
+  //
+  //     var number = 0;
 //     int intValue = 42;
 //     double doubleValue = 12.22;
 //     num value = getValueFromServer();
@@ -45,21 +35,57 @@ void main(){
 //     return 12.88;
 }
 
-getRandomNumber(min, max){
-    var rn = new Random();
-    return min + rn.nextInt(max - min);
+getRandomNumber(min, max) {
+  var rn = new Random();
+  return min + rn.nextInt(max - min);
 }
 
-comparisonOfNumber(int hiddenNumber,int inputUserNumber){
-    String answer = "";
-    if (hiddenNumber == inputUserNumber){
-        answer = "OK";
+comparisonOfNumber(int hiddenNumber, int inputUserNumber) {
+  String answer = "";
+  if (hiddenNumber == inputUserNumber) {
+    answer = "OK";
+  } else if (hiddenNumber > inputUserNumber) {
+    answer = "меньше";
+  } else if (hiddenNumber < inputUserNumber) {
+    answer = "больше";
+  }
+  return answer;
+}
+
+userGuess(hiddenNumber) {
+  int steps = 0;
+  print(hiddenNumber);
+  for (;;) {
+    steps += 1;
+    stdout.write("Введите число: ");
+    int inputUserNumber = int.parse(stdin.readLineSync()!);
+    var answer = comparisonOfNumber(hiddenNumber, inputUserNumber);
+    if (answer == 'OK') {
+      print('Вы выиграли за $steps хода');
+      break;
+    } else {
+      print('Число $answer загаданного');
     }
-    else if (hiddenNumber > inputUserNumber){
-        answer = "меньше";
+  }
+}
+
+compGuess() {
+  int steps = 0;
+  var min = 0;
+  var max = 101;
+  for (;;) {
+    steps += 1;
+    print('$min $max');
+    var number = getRandomNumber(min, max);
+    print('Это число $number?');
+    var userAnswer = stdin.readLineSync();
+    if (userAnswer == 'less') {
+      max = number;
+    } else if (userAnswer == 'greater') {
+      min = number + 1;
+    } else if (userAnswer == 'yes') {
+      print('Компьютер угадал за $steps хода');
+      break;
     }
-    else if (hiddenNumber < inputUserNumber){
-         answer = "больше";
-    }
-    return answer;
+  }
 }
